c38c02c4fe44c7480fcd062a1eab4c24c1cce897
Him10Agr <himanshuagr.c2@gmail.com>
2024-03-10 22:07:08 +0530

    base structure, make_dataset.py and build_features.py configured

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,35 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import pathlib
+import sys
 
+def load_csv(file_path) -> pd.DataFrame:
+    df = load_csv(file_path)
+    return df
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
+def save_csv(train_df: pd.DataFrame, test_df: pd.DataFrame, file_path: str):
+    train_df.to_csv(file_path + 'train.csv')
+    test_df.to_csv(file_path + 'test.csv')
+    
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    
+    cur_dir = pathlib.Path(__file__)
+    home_dir = cur_dir.parent.parent.parent
+    #print(home_dir)
+    sys.path.append(home_dir.as_posix())
+    #print(sys.path)
+    from src.features.build_features import feature_build, feature_drop
+    
+    input_path = sys.argv[1]    #'/data/raw'
+    data_path = home_dir.as_posix() + input_path
+    
+    output_path = home_dir.as_posix() + '/data/processed'
+    
+    train_df = load_csv(data_path + '/train.csv')
+    test_df = load_csv(data_path + '/test.csv')
+    
+    train_data = feature_build(train_df)
+    test_data = feature_build(test_df)
+    
+    save_csv(train_df=train_data, test_df = test_df, file_path=output_path)
+    
+    
---
+++ src/features/build_features.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import pathlib
+
+def correct_datetime(df: pd.DataFrame):
+    df['pickup_datetime'] = pd.to_datetime(df.pickup_datetime)
+    df.loc[:, 'pickup_date'] = df['pickup_datetime'].dt.date
+    df['store_and_fwd_flag'] = 1 * (df.store_and_fwd_flag.values == 'Y')
+
+def haversine_array(lat1, lng1, lat2, lng2):
+    lat1, lng1, lat2, lng2 = map(np.radians, (lat1, lng1, lat2, lng2))
+    AVG_EARTH_RADIUS = 6371  # in km
+    lat = lat2 - lat1
+    lng = lng2 - lng1
+    d = np.sin(lat * 0.5) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(lng * 0.5) ** 2
+    h = 2 * AVG_EARTH_RADIUS * np.arcsin(np.sqrt(d))
+    return h
+
+def dummy_manhattan_distance(lat1, lng1, lat2, lng2):
+    a = haversine_array(lat1, lng1, lat1, lng2)
+    b = haversine_array(lat1, lng1, lat2, lng1)
+    return a + b
+
+def bearing_array(lat1, lng1, lat2, lng2):
+    AVG_EARTH_RADIUS = 6371  # in km
+    lng_delta_rad = np.radians(lng2 - lng1)
+    lat1, lng1, lat2, lng2 = map(np.radians, (lat1, lng1, lat2, lng2))
+    y = np.sin(lng_delta_rad) * np.cos(lat2)
+    x = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(lng_delta_rad)
+    return np.degrees(np.arctan2(y, x))
+
+def create_dist_feature_df(df: pd.DataFrame):
+    df.loc[:, 'distance_haversine'] = haversine_array(df['pickup_latitude'].values, df['pickup_longitude'].values, df['dropoff_latitude'].values, df['dropoff_longitude'].values)
+    df.loc[:, 'distance_dummy_manhattan'] = dummy_manhattan_distance(df['pickup_latitude'].values, df['pickup_longitude'].values, df['dropoff_latitude'].values, df['dropoff_longitude'].values)
+    df.loc[:, 'direction'] = bearing_array(df['pickup_latitude'].values, df['pickup_longitude'].values, df['dropoff_latitude'].values, df['dropoff_longitude'].values)
+    
+
+def test(df):
+    correct_datetime(df)
+    create_dist_feature_df(df)
+    print(df.head())
+    
+def feature_build(df: pd.DataFrame) -> pd.DataFrame:
+    correct_datetime(df)
+    create_dist_feature_df(df)
+    return df
+
+def feature_drop(df: pd.DataFrame, feature_list_drop: list) -> pd.DataFrame:
+    feature_name = [f for f in df.columns if f not in feature_list_drop]
+    df.drop(columns=feature_name, inplace=True)
+    
+if __name__ == '__main__':
+    curr_dir = pathlib.Path(__file__)
+    home_dir = curr_dir.parent.parent.parent
+    data_path = home_dir.as_posix() + '/data/raw/train.csv'
+    
+    data = pd.read_csv(data_path)
+    test(data)
